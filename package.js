c4b2bd49ce5c460c29213cae610111b808893d53
Abigail Watson <awatson1978@gmail.com>
2017-07-23 16:27:23 -0500

    routes; mainIndex; components

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-goal',
-  version: '1.2.7',
+  version: '1.2.8',
   summary: 'HL7 FHIR Resource - Goal',
   git: 'https://github.com/clinical-meteor/hl7-resource-goal',
   documentation: 'README.md'
@@ -17,6 +17,7 @@ Package.onUse(function (api) {
 
   api.use('clinical:base-model@1.3.5');
   api.use('clinical:hl7-resource-datatypes@3.0.0');
+  api.use('clinical:hl7-resource-bundle@1.3.10');
 
   api.addFiles('lib/hl7-resource-goal.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
