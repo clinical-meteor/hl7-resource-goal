6b35647a56079ca8601cb05a901fe877db40bbb4
Abigail Watson <abigailwatson@Abigails-Mac-mini-317772.local>
2018-07-01 23:00:35 -0500

    circle 2.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-goal',
-  version: '1.2.13',
+  version: '1.2.14',
   summary: 'HL7 FHIR Resource - Goal',
   git: 'https://github.com/clinical-meteor/hl7-resource-goal',
   documentation: 'README.md'
