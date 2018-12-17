c30a445e2611328627f113e482fc86d377674301
Abigail Watson <abigailwatson@Abigails-Mac-mini-380428.local>
2018-12-16 20:04:32 -0600

    internal & external features complete; starting QA

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-goal',
-  version: '1.3.7',
+  version: '1.3.8',
   summary: 'HL7 FHIR Resource - Goal',
   git: 'https://github.com/clinical-meteor/hl7-resource-goal',
   documentation: 'README.md'
